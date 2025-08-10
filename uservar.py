478c66a0d53bd1ed3ab5cc200ced3d5994f28659
ballerina <3lei@proton.me>
2025-08-10 16:28:20 +0200

    Update uservar.py

--- uservar.py
+++ uservar.py
@@ -12,3 +12,15 @@ changelog_dir  = 'http://CHANGEME/'
 
 '''#####-----Excludes-----#####'''
 excludes  = ['plugin.video.whatever']
+
+#Change Build Name(s) Guide
+'''
+1. Add your Old/New build names to the BUILDS variable below.
+2. Update your builds file to remove the Old Build and add the New Build.
+   IMPORTANT: Apply the Old Build version to the New Build entry.
+3. Bump the version of your New Build.
+4. At first run the user will be prompted to install the update (Your New Build).'''
+
+#Change Build Name(s)
+BUILDS = [{'Old Build': "Old Build_1", 'New Build': "New Build_1"},
+          {'Old Build': "Old Build_2", 'New Build': "New Build_2"}]
